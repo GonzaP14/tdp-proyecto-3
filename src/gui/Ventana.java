0eb170ba4f00b6acde7033445bd1caedd9f9ece7
Albano Mazzino <albimazzino@gmail.com>
2021-11-01 23:55:10 -0300

    Ventana, Launcher (partially), AnimacionMovimiento, AnimationController added

--- src/gui/Ventana.java
+++ src/gui/Ventana.java
@@ -1,5 +1,97 @@
 package gui;
 
-public class Ventana {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import entidadesGraficas.AnimacionMovimiento;
+import entidadesGraficas.AnimationController;
+import entidadesGraficas.EntidadGrafica;
+import entidadesGraficas.PrincipalGrafico;
+import entidadesLogicas.Juego;
+
+
+public class Ventana extends JFrame implements KeyListener {
+
+	private static final long serialVersionUID = 1L;
+	private Juego miJuego;
+	private EntidadGrafica miPrincipal;
+	private static AnimationController animationControl;
 	
+	
+	public Ventana(Juego miJuego) {
+		this.miJuego = miJuego;
+		animationControl = new AnimationController();
+		miPrincipal = new PrincipalGrafico();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+          case KeyEvent.VK_SPACE:
+              break;
+          case KeyEvent.VK_UP:
+          	if (animationControl.getAnimation() != null && !animationControl.getAnimation().isAnimationFinished()) {
+          		animationControl.getAnimation().setSentidoAnimacion(1);
+          	}
+          	else {
+                	animationControl.addMovement(new AnimacionMovimiento(1, juancito.label2.getBounds().x, juancito.label2.getBounds().y));
+          	}
+            break;
+        case KeyEvent.VK_DOWN:
+        	if (animationControl.getAnimation() != null && !animationControl.getAnimation().isAnimationFinished()) {
+          		animationControl.getAnimation().setSentidoAnimacion(2);
+          	}
+          	else {
+          		animationControl.addMovement(new AnimacionMovimiento(2, juancito.label2.getBounds().x, juancito.label2.getBounds().y));
+          	}
+            break;
+          case KeyEvent.VK_LEFT:
+          	if (animationControl.getAnimation() != null && !animationControl.getAnimation().isAnimationFinished()) {
+          		animationControl.getAnimation().setSentidoAnimacion(3);
+          	}
+          	else {
+          		animationControl.addMovement(new AnimacionMovimiento(3, juancito.label2.getBounds().x, juancito.label2.getBounds().y));
+          	}
+            break;
+          case KeyEvent.VK_RIGHT:
+          	if (animationControl.getAnimation() != null && !animationControl.getAnimation().isAnimationFinished()) {
+          		animationControl.getAnimation().setSentidoAnimacion(4);
+          	}
+          	else {
+          		animationControl.addMovement(new AnimacionMovimiento(4, juancito.label2.getBounds().x, juancito.label2.getBounds().y));
+          	}
+            break;
+         
+          case KeyEvent.VK_P:
+
+          	break;
+          case KeyEvent.VK_R:
+
+          	break;
+          case KeyEvent.VK_M:	            
+//          	if(clip.isRunning()) {
+//          		clip.stop();
+//          	}
+//          	else if(!clip.isRunning()){
+//          		clip.loop(Clip.LOOP_CONTINUOUSLY);
+//          		clip.start();
+//          	}
+          	break;
+      }
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,9 +1,26 @@
 package launcher;
 
+import entidadesLogicas.Juego;
+import gui.Ventana;
+
 public class Launcher {
+	private static int dominioJuego;
+	private static Juego miJuego;
+	private static Ventana miVentana;
 	
 	public static void main(String[] args) {
+		
+		miJuego = new Juego(dominioJuego);
+		miVentana = new Ventana(miJuego);
+		miVentana.setVisible(true);
+	}
 	
+	public void setDominioJuego(int dominio) {
+		dominioJuego = dominio;
+	}
+	
+	public void iniciarJuego() {
+		
 	}
 
 }
