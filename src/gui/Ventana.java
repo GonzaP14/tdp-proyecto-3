9587e7fe3d30d7d84baa23b8a9b91914b2ebb2d7
Albano Mazzino <albimazzino@gmail.com>
2021-11-03 18:02:40 -0300

    Ventana que anda (aberturas ayala)

--- src/gui/Ventana.java
+++ src/gui/Ventana.java
@@ -2,35 +2,25 @@ package gui;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
-
-import entidadesGraficas.AnimacionMovimiento;
-import entidadesGraficas.AnimationController;
 import entidadesGraficas.EntidadGrafica;
 import entidadesGraficas.Laberinto;
-import entidadesGraficas.PrincipalGrafico;
 import entidadesLogicas.Entidad;
-import entidadesLogicas.Grilla;
 import entidadesLogicas.Juego;
-import personajes.Principal;
-
 
 public class Ventana extends JFrame {
 
     private static final long serialVersionUID = 1L;
     private Juego miJuego;
     private EntidadGrafica miPrincipalGrafico;
-    private Principal miPrincipal;
+
     private Laberinto miLaberinto;
     private JLayeredPane layeredPane;
     
     public Ventana(Juego miJuego) {
         this.miJuego = miJuego;
-        miPrincipal = miJuego.getMiPersonajePrincipal();
-        
+        miPrincipalGrafico = miJuego.getMiPersonajePrincipal().getMiRepresentacion();
         miLaberinto = new Laberinto (miJuego.getMiGrilla());
         miLaberinto.setLocation(10, 0);
 		miLaberinto.setSize(726, 794);
@@ -41,20 +31,7 @@ public class Ventana extends JFrame {
     
     private void initialize() {
     	layeredPane = new JLayeredPane();
-	    layeredPane.setBounds(0,0,1250,850);
-//	    ImageIcon[] imagenes = new ImageIcon[5];
-//		imagenes[0] = new ImageIcon(this.getClass().getResource("/recursosMarioBros/MarioArriba.gif"));
-//		imagenes[1] = new ImageIcon(this.getClass().getResource("/recursosMarioBros/MarioAbajo.gif"));
-//		imagenes[2] = new ImageIcon(this.getClass().getResource("/recursosMarioBros/MarioDerecha.gif"));
-//		imagenes[3] = new ImageIcon(this.getClass().getResource("/recursosMarioBros/MarioIzquierda.gif")); 
-//		imagenes[4] = new ImageIcon(this.getClass().getResource("/recursosMarioBros/MarioMuerto.gif")); 
-//	    miPrincipalGrafico = new PrincipalGrafico(imagenes);
-//	    miPrincipal.setMiRepresentacion(miPrincipalGrafico);
-//	    System.out.println(miPrincipalGrafico);
-//	    System.out.println(miJuego);
-	    
-	    
-	    
+	    layeredPane.setBounds(0,0,1250,850); 
 	    layeredPane.add(miLaberinto,Integer.valueOf(0));
 	    layeredPane.add(miPrincipalGrafico, Integer.valueOf(1)); // Principal // 3 -> Fantasas
 	    this.getContentPane().add(layeredPane);
