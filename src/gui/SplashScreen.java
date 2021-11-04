4602705a7a753fb48467d8be91a04123a13c0e69
Brun0Mand0lesi <brunomandol@gmail.com>
2021-11-04 01:54:28 -0300

    splashscreen memoji

--- src/gui/SplashScreen.java
+++ src/gui/SplashScreen.java
@@ -22,7 +22,7 @@ public class SplashScreen extends JDialog{
 		
 		
 		marioButton = new JButton("");
-		marioButton.setBounds(309, 244, 125, 51);
+		marioButton.setBounds(328, 246, 125, 51);
 		ImageIcon botonMario = new ImageIcon(this.getClass().getResource("/recursosMarioBros/botonPlayMario.png"));
 		marioButton.setIcon(new ImageIcon(botonMario.getImage().getScaledInstance(marioButton.getWidth(), marioButton.getHeight(), Image.SCALE_DEFAULT)));
 		marioButton.addActionListener(new ActionListener() {
@@ -34,30 +34,36 @@ public class SplashScreen extends JDialog{
 		this.getContentPane().add(marioButton);
 		
 		// Memoji
-		memojiButton = new JButton("MEMOJI ");
+		memojiButton = new JButton("");
+		memojiButton.setBounds(1078, 246, 125, 51);
+		ImageIcon botonMemoji = new ImageIcon(this.getClass().getResource("/recursosMemoji/botonPlayMemoji.png"));
+		memojiButton.setIcon(new ImageIcon(botonMemoji.getImage().getScaledInstance(memojiButton.getWidth(), memojiButton.getHeight(), Image.SCALE_DEFAULT)));
 		memojiButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dominioJuego.setDominioJuego(DominioJuego.dominioMemoji);
 				setVisible(false);
 			}
 		});
-		memojiButton.setBounds(700, 200, 89, 23);
+		
 		getContentPane().add(memojiButton);
 		
 		
 		
-		ImageIcon fotoMario = new ImageIcon(this.getClass().getResource("/recursosMarioBros/portada.jpg"));
+		ImageIcon fotoMario = new ImageIcon(this.getClass().getResource("/recursosMarioBros/portadaMario.jpg"));
 		JLabel lblFotoMario = new JLabel();
-		lblFotoMario.setBounds(0, 0, 700, 500);
+		lblFotoMario.setBounds(0, 0, 750, 500);
 		lblFotoMario.setIcon(new ImageIcon(fotoMario.getImage().getScaledInstance(lblFotoMario.getWidth(), lblFotoMario.getHeight(), Image.SCALE_DEFAULT)));
 		this.getContentPane().add(lblFotoMario);
 		
+		ImageIcon fotoMemoji = new ImageIcon(this.getClass().getResource("/recursosMemoji/portadaMemoji.jpg"));
 		JLabel lblFotoMemoji = new JLabel();
-		lblFotoMemoji.setBounds(393, 102, 89, 98);
+		lblFotoMemoji.setBounds(750, 0, 750, 500);
+		lblFotoMemoji.setIcon(new ImageIcon(fotoMemoji.getImage().getScaledInstance(lblFotoMemoji.getWidth(), lblFotoMemoji.getHeight(), Image.SCALE_DEFAULT)));
+		
 		getContentPane().add(lblFotoMemoji);
 		
 		
-		this.setBounds(0, 0, 1001, 501);
+
 		
 	}
 }
