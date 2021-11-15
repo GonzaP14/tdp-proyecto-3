0604b92d6105551f3f6f12339109006a5672fd05
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2021-11-15 19:38:52 -0300

    Habia dejado un error en reloj no se xq

--- src/entidadesLogicas/Reloj.java
+++ src/entidadesLogicas/Reloj.java
@@ -22,12 +22,12 @@ public class Reloj extends Thread{
 						miJuego.spawnearFruta();
 					if(ticks == ( 200 + miJuego.getNivel().getDuracionFrutas() ) )
 						miJuego.despawnearFruta();
-					if(ticks == 1) {
+					if(ticks == 400) {
 						miJuego.spawnearPocionCongelacion();
 					}
 					if(ticks == 400 + miJuego.getNivel().getDuracionPociones())
 						miJuego.despawnearPocionCongelacion();
-					if(ticks == 1) {
+					if(ticks == 600) {
 						miJuego.spawnearPocionVelocidad();
 					}
 					if(ticks == 600 + miJuego.getNivel().getDuracionPociones())
