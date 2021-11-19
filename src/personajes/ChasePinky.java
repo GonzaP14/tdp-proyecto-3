dde461dca7f7e08c4d94a8e414057cb57adb5941
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2021-11-19 17:05:38 -0300

    Chase Pinky

--- src/personajes/ChasePinky.java
+++ src/personajes/ChasePinky.java
@@ -10,6 +10,13 @@ public class ChasePinky extends ChaseIA {
 		int sentidoPrincipal = principal.getSentidoActual();
 		Posicion posicionPrincipal = principal.getPosicion();
 		Posicion siguientePosicion;
+		Posicion toReturn = miEnemigo.getPosicion();
+		double distanciaMinima = Double.MAX_VALUE; 
+		/* Asumo que la distancia actual con el principal es la maxima posible.
+		   Esto se realiza para no quedar que el enemigo e no quede en la misma posición, ya que cualquier
+		   otra distancia sera menor con respecto a esta.
+		*/ 
+		double distanciaActual;
 		if(sentidoPrincipal == Entidad.sentidoAbajo ) {
 			siguientePosicion = new Posicion(posicionPrincipal.getX() , posicionPrincipal.getY() + 4 );
 		}
@@ -22,7 +29,19 @@ public class ChasePinky extends ChaseIA {
 		else {
 			siguientePosicion = new Posicion(posicionPrincipal.getX() + 4 , posicionPrincipal.getY() );
 		}
-		return siguientePosicion;
+		
+		
+		for (Posicion pos: miEnemigo.posicionesDestino()) { // Direcciones posibles de pinky
+			distanciaActual = pos.distanciaEntrePosicionesPitagoras(siguientePosicion);
+			
+			if (distanciaActual < distanciaMinima) {
+				distanciaMinima = distanciaActual;
+				toReturn = pos;
+			}
+			
+		}
+		
+		return toReturn;
 	}
 
 }
